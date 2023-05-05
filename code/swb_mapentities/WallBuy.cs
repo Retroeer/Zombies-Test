f174b29b5da377661f71fa30de1507090582c761
Retro <retroneer128@gmail.com>
2023-05-05 19:42:06 -0400

    Create WallBuy.cs

---
+++ code/swb_mapentities/WallBuy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Editor;
+using System.ComponentModel.DataAnnotations;
+using SWB_Player;
+using SWB_Base;
+using SWB_HUD;
+
+namespace Sandbox.maptriggers
+{
+    [HammerEntity]
+    [Display(Name = "Wall Buy", GroupName = "Triggers", Description = "Lets the Player purchase a weapon off the wall")]
+
+    public partial class WallBuy : BaseTrigger
+    {
+
+        [Property(Title = "WallBuyWeapon")]
+        public string WallBuyWeapon { get; set; }
+
+        public WallBuy() { }
+
+        public ExampleInventory Inventory { get; private set; }
+
+        public override void StartTouch(Entity other)
+        {
+            base.StartTouch(other);
+
+            if (!Game.IsServer) return;
+            if (other is not ExamplePlayer) return;
+
+            Log.Error("Touched tehe:)");
+
+
+            if (Input.Pressed("use"))
+            {
+                Inventory.Add(new SWB_WEAPONS.(WallBuyWeapon));
+            }
+        }
+    }
+}
