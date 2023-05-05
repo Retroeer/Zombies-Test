1726e7bbeb5a66669973745e0b8e0ec4c30fbfb4
Retro <retroneer128@gmail.com>
2023-05-05 17:53:57 -0400

    Initial Commit

---
+++ code/ExampleGame.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+using SWB_HUD;
+
+[Library("swb")]
+public partial class ExampleGame : GameManager
+{
+    public HUD UI;
+
+    public ExampleGame()
+    {
+        if (Game.IsServer)
+        {
+            UI = new HUD();
+        }
+    }
+
+    public override void ClientJoined(IClient client)
+    {
+        base.ClientJoined(client);
+
+        var player = new ExamplePlayer(client);
+        player.Respawn();
+
+        client.Pawn = player;
+    }
+}
